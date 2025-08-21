3d8a4bd75cc2e858117e6b26fc9b055afc524f4a
FAF <fridvinf@hiof.no>
2025-08-21 19:35:22 +0200

    Tester pa testinggrounds

---
+++ Testinggrounds/mytestjavascript.js
@@ -0,0 +1,33 @@
+const button = document.getElementById("colorSwap");
+const mytext = document.getElementById("mytext")
+
+let isBlack = true;
+let clicked = false;
+
+button.addEventListener("click", function() {
+    if (clicked) {
+        button.textContent = "Light";
+
+    } else {
+        button.textContent = "Dark";
+    }
+    clicked = !clicked;
+})
+
+button.addEventListener("click", () => {
+    if (isBlack) {
+        document.body.style.backgroundColor = "white";
+        document.body.style.Color = "black";
+        mytext.style.color = "black";
+        isBlack = false;
+    } else {
+        document.body.style.backgroundColor = "black";
+        document.body.style.Color = "white";
+        mytext.style.color = "white";
+        isBlack = true;
+    }
+})
+
+button.style.position = "absolute"
+button.style.left = "620px"
+button.style.bottom = "100px"
